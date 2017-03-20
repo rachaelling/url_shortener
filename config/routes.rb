b00b77b9c7b3912b96a50cb350d37807981df4cb
rachaelling <rachaellhe2@hotmail.com>
2017-03-20 11:40:54 +0000

    set up routes for url model and set urls/index as root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root 'urls#index'
+  #this sets urls/index as root page
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :urls
 end
