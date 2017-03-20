08325b5a57686c7bc77e75ae2e6599856895e075
rachaelling <rachaellhe2@hotmail.com>
2017-03-20 15:05:34 +0000

    set up validations and shorten method in the url model

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,9 @@
-class Url < ApplicationRecord
+class Url < ActiveRecord::Base
+validates :long_url, format: {with: (URI::regexp(['http', 'https'])), message: "you have entered an invalid URL"}
+  validates :long_url, uniqueness: true
+  before_create :shorten
+  
+  def shorten
+    self.short_url = SecureRandom.hex(5)
+  end
 end
