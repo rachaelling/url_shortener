5dd4baa33bb7b2d22ea1c1bab6b944703ba30de8
rachaelling <rachaellhe2@hotmail.com>
2017-03-20 17:33:39 +0000

    Created new and create actions for urls

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -8,4 +8,24 @@ def show
   @url = Url.find(params[:id])
 end
 
+def new
+  @url = Url.new
+end
+
+def create
+  @url = Url.new(url_params)
+  @url.shorten
+    if @url.save
+      redirect_to @url
+    else
+      render 'new'
+    end
+end
+
+# private method that only permits long_url to be exposed
+private
+def url_params
+  params.require(:url).permit(:long_url)
+end
+
 end
